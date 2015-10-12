d468e4c652e1739d5c37fb3db4443200e411d5b7
Jake Karnes <jake.karnes@gmail.com>
2015-10-12 15:47:58 -0700

    Finishing emoji parser. Adds documents to MongoDB.

--- emoji/emoji_parser.js
+++ emoji/emoji_parser.js
@@ -1,11 +1,12 @@
-/*
-TODO: Make sure Schema definition is complete in db.js
-Take parsed data and save to DB via Mongoose model.
-*/
-
+//Cheerio for parsing HTML
 var $ = require('cheerio')
+//fs for saving images to disk
 var fs = require('fs')
-var db = require('../server/db.js')
+//Connect to MonogDB via Mongoose
+var db = require('../server/model/db.js')
+//Get our custom Emoji Model
+var mongoose = require( 'mongoose' )
+var Emoji = mongoose.model('Emoji')
 
 //Load/Parse HTML from disk
 var htmlString = fs.readFileSync('Full_Emoji_Data.html').toString()
@@ -29,32 +30,58 @@ function parseEmojiRow(index, emojiRow) {
   //Array of all annotations (tags) for this emoji
   var annotations = emojiRow.children().eq(15).text().split(', ')
 
-  // save black and white flavor
-  var basicFileName = saveEmojiFlavorImage(name, 'basic', emojiRow.children().eq(3).find('img'))
+  //An initially empty array of all the flavors we found.
+  var flavors = []
 
-  // save Apple flavor
-  var appleFileName = saveEmojiFlavorImage(name, 'apple', emojiRow.children().eq(4).find('img'))
+  //Add black and white flavor 
+  addEmojiFlavor(emojiRow, name, 'basic', 3, flavors)
+
+  // add Apple flavor
+  addEmojiFlavor(emojiRow, name, 'apple', 4, flavors)
 
   // save Android flavor
-  var androidFileName = saveEmojiFlavorImage(name, 'android', emojiRow.children().eq(5).find('img'))
+  addEmojiFlavor(emojiRow, name, 'android', 5, flavors)
 
   // save Twitter flavor
-  var twitterFileName = saveEmojiFlavorImage(name, 'twitter', emojiRow.children().eq(6).find('img'))
+  addEmojiFlavor(emojiRow, name, 'twitter', 6, flavors)
 
   // save Windows flavor
-  var windowsFileName = saveEmojiFlavorImage(name, 'windows', emojiRow.children().eq(7).find('img'))
+  addEmojiFlavor(emojiRow, name, 'windows', 7, flavors)
+
+  //Save to database
+  Emoji.create({ 
+    name: name, 
+    unicode: unicode, 
+    annotations: annotations,
+    flavors: flavors
+    }, function (err, emoji) {
+      if (err) return handleError(err);
+    // saved!
+  })
 
 }
 
-function saveEmojiFlavorImage(emojiName, flavorName, imgElement){
-	var imageSrcElement = imgElement.attr('src')
-	if(typeof imageSrcElement !== "undefined")
-	{
-		var imageName = emojiName+'|'+flavorName+'.png'
-  		var base64Data = imageSrcElement.replace(/^data:image\/png;base64,/, "");
-		fs.writeFileSync('images/' + imageName, base64Data, 'base64', function(err) {console.log(err);});
-		console.log('Saved ' + imageName)
-		return imageName
-	}
+// Checks if this flavor exists for this emoji. 
+// If it does exist, it saves the flavor image to disk and adds metadata to flavors array.
+function addEmojiFlavor(emojiRow, emojiName, flavorName, columnIndex, flavors){
+  var imageElement = emojiRow.children().eq(columnIndex).find('img')
+  var imageSrcElement = imageElement.attr('src')
 
+  //Only continue if this flavor exists. Otherwise, do nothing.
+  if(typeof imageSrcElement !== "undefined"){
+    var imageName = emojiName+'|'+flavorName+'.png'
+    //Save image to disk
+    saveEmojiFlavorImage(imageName, imageSrcElement)
+    //Add to array of found flavors.
+    flavors.push({flavorName: flavorName, fileName:imageName})
+  } 
 }
+
+//Save the Base64 encoded PNG to disk and return the filename.
+function saveEmojiFlavorImage(imageName, imageSrcElement){
+ var base64Data = imageSrcElement.replace(/^data:image\/png;base64,/, "");
+ fs.writeFileSync('images/' + imageName, base64Data, 'base64', function(err) {console.log(err);});
+ console.log('Saved ' + imageName)
+}
+
+console.log('All done with emojis.')
